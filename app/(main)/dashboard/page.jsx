3d00d6ac3046d6f9b6776756c72455af37a864ad
Swapnadeep Roy <swapnadeeproy672@gmail.com>
2025-08-19 02:28:03 +0530

    Make dashboard page pure server component to fix crash

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,8 +1,9 @@
+// This is a Server Component now
+
 import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import React, { Suspense } from "react";
 import AccountCard from "./_components/account-card";
 import { getCurrentBudget } from "@/actions/budget";
 import BudgetProgress from "./_components/budget-progress";
@@ -10,10 +11,8 @@ import DashboardOverview from "./_components/transaction-overview";
 
 async function DashboardPage() {
     const accounts = await getUserAccounts();
-
     const defaultAccount = accounts?.find((account) => account.isDefault);
 
-    // Get budget for default account
     let budgetData = null;
     if (defaultAccount) {
         budgetData = await getCurrentBudget(defaultAccount.id);
@@ -23,23 +22,18 @@ async function DashboardPage() {
 
     return (
         <div className="space-y-8">
-            {/* Budget Progress */}
-            { defaultAccount && (
-            <BudgetProgress
-                initialBudget={budgetData?.budget}
-                currentExpenses={budgetData?.currentExpenses || 0}
-            />
+            {defaultAccount && (
+                <BudgetProgress
+                    initialBudget={budgetData?.budget}
+                    currentExpenses={budgetData?.currentExpenses || 0}
+                />
             )}
 
-            {/* Overview */}
-            <Suspense fallback={"Loading Overview..."}>
-                <DashboardOverview
-                    accounts={accounts}
-                    transactions={transactions || []}
-                />
-            </Suspense>
+            <DashboardOverview
+                accounts={accounts}
+                transactions={transactions || []}
+            />
 
-            {/* Accounts Grid */}
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                 <CreateAccountDrawer>
                     <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
@@ -50,9 +44,10 @@ async function DashboardPage() {
                     </Card>
                 </CreateAccountDrawer>
 
-                {accounts.length>0 && accounts?.map((account) =>{
-                    return <AccountCard key={account.id} account={account} />;
-                })}
+                {accounts.length > 0 &&
+                    accounts?.map((account) => (
+                        <AccountCard key={account.id} account={account} />
+                    ))}
             </div>
         </div>
     );
