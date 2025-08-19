39a2762c9b3640b6402307a231ec74685e80fbaf
Swapnadeep Roy <swapnadeeproy672@gmail.com>
2025-08-19 09:17:05 +0530

    Fix next.config.js export and pathname for remote image

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "randomuser.me",
-        },
-      ],
-    },
-
-    experimental: {
-      serverActions: {
-        bodySizeLimit: "5mb",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/**",   // required
       },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
     },
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
